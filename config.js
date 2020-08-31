9895f6bd5c61f9db25ed330f299807f15935a6a5
v2rayii <70507685+v2rayii@users.noreply.github.com>
2020-08-31 15:25:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'proxy.netptop.com',
+    serverName: 'proxy.v2rayii.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
